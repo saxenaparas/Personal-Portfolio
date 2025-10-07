707bfca4898676aae55d687c06daa631241a4fce
saxenaparas <parassaxenaofficial@gmail.com>
2025-10-08 02:26:40 +0530

    🔧 fix(footer): resolved layout and alignment issues

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ import SocialMediaLinks from "./ui/SocialMediaLinks";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white ">
-      <div className="container pl-24 pr-0 p-7 flex justify-between">
+      <div className="container p-7 flex justify-between">
         <Logo/>
         <SocialMediaLinks />
       </div>
